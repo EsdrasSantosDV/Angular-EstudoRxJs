072bdb6e93893fb00387c7c28398be7d50d0fc82
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-09-13 16:41:16 -0300

    Operadores Filter debounce distinct

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -3,7 +3,8 @@ import { FormControl } from '@angular/forms';
 import {Acoes} from './modelo/acoes';
 import {AcoesService} from './acoes.service';
 import { merge, Subscription } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+const ESPERA_DIGITACAO=300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
@@ -13,7 +14,13 @@ export class AcoesComponent {
 
   acoesInput = new FormControl();
   todasAcoes$=this.acoesService.getAcoes().pipe(tap(console.log));
-  filtroPeloInput$=this.acoesInput.valueChanges.pipe(tap(()=>{console.log('Fluxo do Filtro')}),switchMap((valorDigitado)=>this.acoesService.getAcoes(valorDigitado)));
+  filtroPeloInput$=this.acoesInput.valueChanges.pipe(
+  debounceTime(ESPERA_DIGITACAO),
+  tap(()=>{console.log('Fluxo do Filtro')}),
+  tap(console.log),
+  filter((valorDigitado)=>valorDigitado.length >=3 || !valorDigitado.length),
+  distinctUntilChanged(),
+  switchMap((valorDigitado)=>this.acoesService.getAcoes(valorDigitado)));
 
 
   //ESSA FUNCAO MERGE COMBINA DOIS FLUXOS INDENPOENDEDNTES
